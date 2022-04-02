e3482f06598ff2d4639251a1d935079f1b7acb2e
Lucas Burlingham <lucas@lucasburlingham.me>
2022-04-02 15:05:38 -0400

    Add readability changes

--- x86_64/scan_repo.sh
+++ x86_64/scan_repo.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# Make the primary server repo database files
 repo-add -n -R jovarkos-repo.db.tar.gz *.pkg.tar.zst
+# Make the backup server repo database files
+repo-add -n -R jovarkos-repo-secondary.db.tar.gz *.pkg.tar.zst
 rm *.gz.old
 git add .
-git commit -m "Add packages"
+git commit -m "[SCAN REPO] Add packages to repo"
 git push 
