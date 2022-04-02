658d9921ed6382f7e1a3482cf71d155e80e7e064
Lucas Burlingham <lucas@lucasburlingham.me>
2022-04-02 18:31:30 -0400

    [SCAN REPO] Add packages to repo

--- x86_64/scan_repo.sh
+++ x86_64/scan_repo.sh
@@ -2,9 +2,7 @@
 
 # Make the primary server repo database files
 repo-add -n -R jovarkos-repo.db.tar.gz *.pkg.tar.zst
-# Make the backup server repo database files
-repo-add -n -R jovarkos-repo-secondary.db.tar.gz *.pkg.tar.zst
 rm *.gz.old
 git add .
 git commit -m "[SCAN REPO] Add packages to repo"
-git push 
+git push
